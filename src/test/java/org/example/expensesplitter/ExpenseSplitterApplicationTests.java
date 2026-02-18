43b09271b22472ef9e5b4b9ada5866736fa14446
sajid <sajid@ubuntu.com>
2026-02-18 13:11:31 +0530

    initial commit

---
+++ src/test/java/org/example/expensesplitter/ExpenseSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.expensesplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSplitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
